cf93c3af5c084d883091e1761ff3539098d2568b
Sai Reddy <nagasaireddy54@gmail.com>
2022-06-10 07:54:15 -0400

    hot fix

--- src/screens/resultScreen.tsx
+++ src/screens/resultScreen.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Container, makeStyles } from "@material-ui/core";
 import { useLocation, useNavigate } from "react-router-dom";
 
-export const ResultScreen: React.FunctionComponent = (props) => {
+export const ResultScreen: React.FunctionComponent = () => {
     const classes = useStyles();
     const navigate = useNavigate();
 
@@ -14,7 +14,7 @@ export const ResultScreen: React.FunctionComponent = (props) => {
         <Container maxWidth="md" className={classes.container}>
             <div>
                 <h2>Your Package has been purchased successfully.</h2>
-                <p>Package Number: {state.packageNo}</p>
+                <p>Package Number: {state.package}</p>
                 <p>Email: {state.email}</p>
                 <p>Age: {state.age}</p>
                 <p>Gender: {state.gender.toLowerCase() === "f" ? "Female" : "Male"}</p>
