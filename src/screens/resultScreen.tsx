2c1f95624044cd1438e9f6830fe702fef6314411
Sai Reddy <nagasaireddy54@gmail.com>
2022-06-09 23:39:01 -0400

    route added

---
+++ src/screens/resultScreen.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button, Container, makeStyles } from "@material-ui/core";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const ResultScreen: React.FunctionComponent = (props) => {
+    const classes = useStyles();
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    const state = location.state as any;
+
+    return (
+
+        <Container maxWidth="md" className={classes.container}>
+            <div>
+                <h2>Your Package has been purchased successfully.</h2>
+                <p>Package Number: {state.packageNo}</p>
+                <p>Email: {state.email}</p>
+                <p>Age: {state.age}</p>
+                <p>Gender: {state.gender.toLowerCase() === "f" ? "Female" : "Male"}</p>
+            </div>
+            <Button variant="contained" color="primary" onClick={() => navigate('/')}>
+                Buy Again
+            </Button>
+        </Container>
+
+    );
+};
+
+const useStyles = makeStyles({
+    container: {
+        marginTop: 30,
+    },
+});
